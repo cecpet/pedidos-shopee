b7de73db91bbda2ca64c699cd9a965e135c11a54
Cec Pet <propaganda@cecpet.com>
2022-11-21 11:05:44 -0300

    Vercel Config

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   
   const [pedidos, setPedidos] = useState([]);
   useEffect(()=> {
-    fetch('https://pedidos-shopee.vercel.app/api')
+    fetch('/api')
     .then(response => response.json())
     .then(data=>{
       setPedidos(data.retorno.pedidos)
