2dbcb6966267497b8d815b48afe45295ee8f761e
Cec Pet <propaganda@cecpet.com>
2022-11-21 10:52:56 -0300

    Botão de pedido pronto

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -1,8 +1,15 @@
 import { Children } from "react";
 
 export default function Pedido(props) {
+
+    function handleClick(item) {
+        item.target.classList.toggle('bg-slate-300')
+        item.target.classList.toggle('bg-green-500')
+    }
+
     return(
-        <div className="sm:mt-12 p-8 border-2 mx-auto w-1/2 rounded-md">
+        <div className="relative w-screen sm:mt-12 p-8 border-2 min-w-full rounded-md">
+            <button className="bg-slate-300 p-2 rounded-lg" onClick={handleClick}>Pronto</button>
             <h1 className="text-2xl">{props.id}</h1>
             <h2>{props.data}</h2>
             <h3>{props.nome}</h3>
