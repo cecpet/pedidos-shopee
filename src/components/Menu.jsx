d66eb2a4f1453b99b39ae0802929a649f00805b6
Cec Pet <propaganda@cecpet.com>
2022-11-21 17:33:37 -0300

    Posicionamento do botão de fechar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,7 @@ export default function Menu(props) {
     return(
         <div className={props.style == '' ? 'wd-full h-full bg-slate-200 md:w-1/3 absolute right-0 -top-8 translate-x-full p-8  z-20' : props.style}>
             <IconContext.Provider value={{size: "2em"}}>
-                <MdOutlineClose className="hover:cursor-pointer ml-96" onClick={props.styleClear}/>
+                <MdOutlineClose className="hover:cursor-pointer mt-6" onClick={props.styleClear}/>
             </IconContext.Provider>
             <h1 className="mt-10 font-bold text-4xl">Pedidos</h1>
             <ul className="my-5 font-medium text-2xl">
