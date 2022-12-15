229dda30c42e2b6fc751733f53174ad88e24c7bb
igutzzz <ofc.iguituu@gmail.com>
2022-12-15 20:23:20 -0300

    ADD Data prevista de envio

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -35,6 +35,26 @@ export default function Pedido(props) {
         }
     }
 
+    let date = new Date(props.data);
+    let dataPedido = (date.getDate() + 1) + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+    let dia = date.getDate() + 1
+    let mes = date.getMonth() + 1;
+    let ano = date.getFullYear();
+
+    function somarData(dias) {
+        let diaPrevisto = dia+5+"/"+mes+"/"+ano;
+    
+        if (dia > 30){
+            let novoDia = dias - (30 - dia);
+            mes++;
+
+            diaPrevisto = novoDia+"/"+mes+"/"+ano;
+            return diaPrevisto
+        }
+
+        return diaPrevisto
+    }
+
     return(
         <div className="relative sm:mt-12 p-8 border-2 min-w-full rounded-md ">
             <div className="flex items-center justify-between">
@@ -43,7 +63,8 @@ export default function Pedido(props) {
             </div>
             <h1 className="text-2xl">{props.id}</h1>
 
-            <h2>{props.data}</h2>
+            <h2>Criado em: <span className="font-bold">{dataPedido}</span></h2>
+            <h2>Entregar até: <span className="font-bold">{somarData(5)}</span></h2>
             <h3>{props.nome}</h3>
             <div id="itens">
                 {props.children}
